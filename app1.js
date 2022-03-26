5b1824d3cfc6ada511288c0eb12b066ff6359b30
shahmeerbaig2 <57468685+shahmeerbaig2@users.noreply.github.com>
2022-03-26 10:45:43 +0500

    changes

--- app1.js
+++ app1.js
@@ -7,14 +7,14 @@ bars.forEach(function(bar){
     console.log(percentage);
 }
 )
-$(document).on({
-    ajaxStart: function(){
-        $("body").addClass("loading"); 
-    },
-    ajaxStop: function(){ 
-        $("body").removeClass("loading"); 
-    }    
-});
+// $(document).on({
+//     ajaxStart: function(){
+//         $("body").addClass("loading"); 
+//     },
+//     ajaxStop: function(){ 
+//         $("body").removeClass("loading"); 
+//     }    
+// });
 
 
 //    $(".button").click(function(){
@@ -24,13 +24,42 @@ $(document).on({
 //     $("div#wrapper").printArea(options);
 
 //    });
-   $('.progress-bar').animate(
-    {width:'100%'},
-    { duration:3000,
-      complete:function(){
-        $('.progress').css('visibility','hidden');
-      }
+  //  $('.progress-bar').animate(
+  //   {width:'100%'},
+  //   { duration:3000,
+  //     complete:function(){
+  //       $('.progress').css('visibility','hidden');
+  //     }
+  //   });
+
+  
+     
+  $(document).ready(function(){
+    $("#header").click(function(){
+        $("#header").css({
+            "background-color":"red"
+        });
     });
+});
+$(document).ready(function(){
+  var body=$('body');
+  var color=[
+    'red',
+    'green',
+    'blue',
+    'yellow'
+  ];
+  var i=0;
+  function changeColor(){
+    body.css('background',color[i]);
+    i++;
+    if(i>=color.length){
+      i=0;
+    }
+  }
+  setInterval(changeColor,1000);
+})
+
 
      
   
